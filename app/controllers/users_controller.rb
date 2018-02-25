d56fa820ece92c8a022c0fafe674076a6e25ad95
George Buckley <g.buckley@live.com>
2018-02-25 15:01:39 +0000

    downcased username

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
     def show
-        @user = User.find_by(username: params[:id])
+        @user = User.find_by(username: params[:id].to_s.downcase)
     end
 end
