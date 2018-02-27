40a7dcfa81a99f5ecbc1cf277a7897bc639a4a30
George Buckley <g.buckley@live.com>
2018-02-27 01:15:00 +0000

    CSS

--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -4,4 +4,5 @@ AssetSync.configure do |config|
     config.gzip_compression = true
     config.google_storage_access_key_id = ENV['GOOGLE_STORAGE_ACCESS_KEY_ID']
     config.google_storage_secret_access_key = ENV['GOOGLE_STORAGE_SECRET_ACCESS_KEY']
+    config.existing_remote_files = 'delete'
 end
